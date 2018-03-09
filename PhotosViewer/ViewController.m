9704246fe5bfecc02507fdf3837b54b3996f3b91
李一贤 <liyixian@atomlee-imac.local>
2018-03-09 18:47:29 +0800

    新增点击上一张/下一张图片时如果点击到头，让该按钮不可点击到的代码实现

--- PhotosViewer/ViewController.m
+++ PhotosViewer/ViewController.m
@@ -11,6 +11,8 @@
 @interface ViewController ()
 - (IBAction)preView;
 - (IBAction)next;
+@property (weak, nonatomic) IBOutlet UIButton *preViewBtn;
+@property (weak, nonatomic) IBOutlet UIButton *nextBtn;
 @property(nonatomic,assign)int index;//图片索引值，用来判断当前是否第一张/最后一张
 @property (weak, nonatomic) IBOutlet UIImageView *headImage;
 @property (weak, nonatomic) IBOutlet UILabel *numLabel;
@@ -47,12 +49,13 @@
             self.numLabel.text =@"1/3";
             self.headImage.image = [UIImage imageNamed:@"1"];
             self.descLabel.text = @"1";
+            self.preViewBtn.enabled = NO;
             break;
         case 1:
             self.numLabel.text =@"2/3";
             self.headImage.image = [UIImage imageNamed:@"2"];
             self.descLabel.text = @"2";
-            
+            self.nextBtn.enabled = YES;
             break;
         case 2:
             self.numLabel.text = @"3/3";
@@ -73,17 +76,19 @@
             self.numLabel.text =@"1/3";
             self.headImage.image = [UIImage imageNamed:@"1"];
             self.descLabel.text = @"1";
+            
             break;
         case 1:
             self.numLabel.text =@"2/3";
             self.headImage.image = [UIImage imageNamed:@"2"];
             self.descLabel.text = @"2";
-            
+            self.preViewBtn.enabled = YES;
             break;
         case 2:
             self.numLabel.text = @"3/3";
             self.headImage.image = [UIImage imageNamed:@"3"];
             self.descLabel.text = @"3";
+            self.nextBtn.enabled =NO;
         default:
             self.index = 2;
             break;
