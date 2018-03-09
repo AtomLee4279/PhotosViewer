25d32d19e3f6f6303c5740ec4361bbae962380ff
李一贤 <liyixian@atomlee-imac.local>
2018-03-09 15:54:57 +0800

    图片浏览器基本功能提交

--- PhotosViewer/ViewController.m
+++ PhotosViewer/ViewController.m
@@ -9,7 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+- (IBAction)preView;
+- (IBAction)next;
+@property(nonatomic,assign)int index;//图片索引值，用来判断当前是否第一张/最后一张
+@property (weak, nonatomic) IBOutlet UIImageView *headImage;
+@property (weak, nonatomic) IBOutlet UILabel *numLabel;
+@property (weak, nonatomic) IBOutlet UILabel *descLabel;
 @end
 
 @implementation ViewController
@@ -26,4 +31,51 @@
 }
 
 
+- (IBAction)preView
+{
+    self.index --;
+    switch (_index) {
+        case 0:
+            self.numLabel.text =@"1/3";
+            self.headImage.image = [UIImage imageNamed:@"1"];
+            self.descLabel.text = @"1";
+            break;
+        case 1:
+            self.numLabel.text =@"2/3";
+            self.headImage.image = [UIImage imageNamed:@"2"];
+            self.descLabel.text = @"2";
+            break;
+        case 2:
+            self.numLabel.text = @"3/3";
+            self.headImage.image = [UIImage imageNamed:@"3"];
+            self.descLabel.text = @"3";
+        default:
+            break;
+    }
+    
+}
+
+- (IBAction)next
+{
+    
+    self.index ++;
+    switch (_index) {
+        case 0:
+            self.numLabel.text =@"1/3";
+            self.headImage.image = [UIImage imageNamed:@"1"];
+            self.descLabel.text = @"1";
+            break;
+        case 1:
+            self.numLabel.text =@"2/3";
+            self.headImage.image = [UIImage imageNamed:@"2"];
+            self.descLabel.text = @"2";
+            break;
+        case 2:
+            self.numLabel.text = @"3/3";
+            self.headImage.image = [UIImage imageNamed:@"3"];
+            self.descLabel.text = @"3";
+        default:
+            break;
+    }
+}
 @end
