e94179ee7b373fca08967198c32fa803324e0f80
李一贤 <liyixian@atomlee-imac.local>
2018-03-10 11:00:27 +0800

    调整优化代码结构，优化浏览图片的操作流程体验

--- PhotosViewer/ViewController.m
+++ PhotosViewer/ViewController.m
@@ -9,8 +9,6 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-- (IBAction)preView;
-- (IBAction)next;
 @property (weak, nonatomic) IBOutlet UIButton *preViewBtn;
 @property (weak, nonatomic) IBOutlet UIButton *nextBtn;
 @property(nonatomic,assign)int index;//图片索引值，用来判断当前是否第一张/最后一张
@@ -24,12 +22,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.index = 0;
-    if (self.headImage.image==nil)
-    {
-        self.numLabel.text =@"1/3";
-        self.headImage.image = [UIImage imageNamed:@"1"];
-        self.descLabel.text = @"1";
-    }
+    self.preViewBtn.enabled = NO;
+    [self setPhotosData];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -38,60 +33,52 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-
-- (IBAction)preView
+//点击前一张/后一张触发事件
+-(IBAction)viewPhotos:(UIButton*)btn
 {
-    self.index --;
-    NSLog(@"preview:index:%@",[NSString stringWithFormat:@"%d",self.index]);
+    (btn.tag == 1)?(self.index--):(self.index++);
+    NSLog(@"viewPhotos:index:%@",[NSString stringWithFormat:@"%d",self.index]);
+    [self setPhotosData];
+    //设置按钮状态，及索引值
     switch (_index) {
         case 0:
-            self.numLabel.text =@"1/3";
-            self.headImage.image = [UIImage imageNamed:@"1"];
-            self.descLabel.text = @"1";
             self.preViewBtn.enabled = NO;
             break;
         case 1:
-            self.numLabel.text =@"2/3";
-            self.headImage.image = [UIImage imageNamed:@"2"];
-            self.descLabel.text = @"2";
-            self.nextBtn.enabled = YES;
+            (btn.tag == 1)?(self.nextBtn.enabled = YES):(self.preViewBtn.enabled = YES);
             break;
         case 2:
-            self.numLabel.text = @"3/3";
-            self.headImage.image = [UIImage imageNamed:@"3"];
-            self.descLabel.text = @"3";
+            self.nextBtn.enabled =NO;
         default:
-            self.index = 0;
+            (btn.tag == 1)?(self.index = 0):(self.index = 2);
+            (btn.tag == 1)?(self.preViewBtn.enabled = NO):(self.nextBtn.enabled = NO);
             break;
     }
+    
 }
-
-- (IBAction)next
+//根据索引值设置图片数据
+-(void)setPhotosData
 {
-    self.index ++;
-    NSLog(@"next:index:%@",[NSString stringWithFormat:@"%d",self.index]);
-    switch (_index) {
+    switch (_index)
+    {
         case 0:
             self.numLabel.text =@"1/3";
             self.headImage.image = [UIImage imageNamed:@"1"];
             self.descLabel.text = @"1";
-            
             break;
         case 1:
             self.numLabel.text =@"2/3";
             self.headImage.image = [UIImage imageNamed:@"2"];
             self.descLabel.text = @"2";
-            self.preViewBtn.enabled = YES;
             break;
         case 2:
             self.numLabel.text = @"3/3";
             self.headImage.image = [UIImage imageNamed:@"3"];
             self.descLabel.text = @"3";
-            self.nextBtn.enabled =NO;
         default:
-            self.index = 2;
             break;
     }
+    
 }
+
 @end
