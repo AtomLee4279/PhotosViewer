14620162e4fe87fb2f74b997a7d921a97edf9d7e
李一贤 <liyixian@atomlee-imac.local>
2018-03-10 14:52:07 +0800

    重写images数组的get方法，使用懒加载进行图片数据加载；优化性能

--- PhotosViewer/ViewController.m
+++ PhotosViewer/ViewController.m
@@ -22,26 +22,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //保存图片数据
-    NSMutableDictionary *dict1 = [NSMutableDictionary dictionary];
-    dict1[@"icon"] = @"1";
-    dict1[@"desc"] = @"1";
-    NSMutableDictionary *dict2 = [NSMutableDictionary dictionary];
-    dict2[@"icon"] = @"2";
-    dict2[@"desc"] = @"2";
-    NSMutableDictionary *dict3 = [NSMutableDictionary dictionary];
-    dict3[@"icon"] = @"3";
-    dict3[@"desc"] = @"3";
-    _images = @[dict1,dict2,dict3];
     self.index = 0;
     self.preViewBtn.enabled = NO;
     [self setPhotosData];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//懒加载（延迟加载）
+//核心思想：当数据不存在的时候加载
+//实现方式：重写get方法
+-(NSArray*)images
+{
+    if(_images ==nil)//注意：此处若用self.images ==nil,那么相当于重写get方法里又调用了get方法。会造成死循环。所以改用成员属性_images描述
+    {
+        //保存图片数据
+        NSMutableDictionary *dict1 = [NSMutableDictionary dictionary];
+        dict1[@"icon"] = @"1";
+        dict1[@"desc"] = @"1";
+        NSMutableDictionary *dict2 = [NSMutableDictionary dictionary];
+        dict2[@"icon"] = @"2";
+        dict2[@"desc"] = @"2";
+        NSMutableDictionary *dict3 = [NSMutableDictionary dictionary];
+        dict3[@"icon"] = @"3";
+        dict3[@"desc"] = @"3";
+        _images = @[dict1,dict2,dict3];
+    }
+    return _images;
 }
 
 //点击前一张/后一张触发事件
