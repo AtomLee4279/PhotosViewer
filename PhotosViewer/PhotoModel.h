5f6d84906fe089eb54a6fed9d3e40f7f998f7e9a
李一贤 <liyixian@liyixiandembp.lan>
2018-03-12 00:12:39 +0800

    图片数据存储形式：字典转模型（MVC模式）的初步创建

---
+++ PhotosViewer/PhotoModel.h
@@ -0,0 +1,17 @@
+//
+//  PhotoModel.h
+//  PhotosViewer
+//
+//  Created by 李一贤 on 2018/3/11.
+//  Copyright © 2018年 李一贤. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PhotoModel : NSObject
+
+@property(nonatomic,copy)NSString* icon;
+@property(nonatomic,copy)NSString* desc;
+
+
+@end
---
+++ PhotosViewer/PhotoModel.m
@@ -0,0 +1,13 @@
+//
+//  PhotoModel.m
+//  PhotosViewer
+//
+//  Created by 李一贤 on 2018/3/11.
+//  Copyright © 2018年 李一贤. All rights reserved.
+//
+
+#import "PhotoModel.h"
+
+@implementation PhotoModel
+
+@end
